4a61d8f6bb6c69cfacdf95191792d63225b329ea
Mita-cpu <82151864+Mita-cpu@users.noreply.github.com>
2021-06-15 18:06:29 +0200

    Added a subtraction function

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function multiply (num1,num2) {
   console.log(multiply);
 }
 multiply(7,8);
+
+function difference (a , b) {
+  return a - b;
+}
+let subtraction = difference(15,3);
+console.log (subtraction);
